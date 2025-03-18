10588614eeb9870c7b12afa345372687f311a510
Victor Portela <168148556+VictorPortela01@users.noreply.github.com>
2025-03-17 21:29:05 -0300

    aula 26

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,8 +1,10 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layouts/Message";
+import styles from "./projects.module.css";
+import Container from "../layouts/Container";
+import LinkButton from "../layouts/LinkButton";
 
 const Projects = () => {
-
   const location = useLocation();
   let message = "";
   if (location.state) {
@@ -10,9 +12,15 @@ const Projects = () => {
   }
 
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="newproject" text="Criar Projeto"/>
+      </div>
       {message && <Message type="success" msg={message} />}
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 };
