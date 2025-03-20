c734bfa6a7b7adbf5c94f6abce3e154123e1c364
Victor <victorportela1010@gmail.com>
2025-03-20 15:40:27 -0300

    Update ProjectForm.jsx

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import Input from "../form/Input";
+import Input from "../form/Input.jsx";
 import styles from "./ProjectForm.module.css";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
